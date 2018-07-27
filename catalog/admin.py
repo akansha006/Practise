456d992c1ec7b0e34487d6b278a82a2880863c77
Akansha Kumari <akanshakumari125@gmail.com>
2018-07-27 21:48:52 +0530

    django

---
+++ catalog/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Genre, Book, BookInstance, Author, Language
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author',)
+    list_filter = ('genre',)
+    fields = [('title', 'author'), 'summary', ('isbn', 'language'), 'genre']
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstance(admin.ModelAdmin):
+    list_display = ('book', 'imprint', 'status','borrower')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        ('Info', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': [('status', 'due_back','borrower')]
+        }),
+    )
+
+
+
+admin.site.register(Genre)
+admin.site.register(Language)
